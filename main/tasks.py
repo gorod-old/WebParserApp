0b642b562a3e5e5a60583188b64b674a884a3475
Alex Gorodetsky <gorod1974@hotmail.com>
2022-03-01 20:40:36 +0300

    commit 1.03.2022

--- main/tasks.py
+++ main/tasks.py
@@ -1,9 +1,18 @@
 from celery import shared_task
+
+from main.models import WorkTable
 from main.parser import parser_info
 
 
 @shared_task
 def run_pars():
+    try:
+        obj = WorkTable.objects.all()[0]
+        spreadsheet = obj.spreadsheet
+    except Exception as e:
+        print(str(e))
+        spreadsheet = None
+    print('spreadsheet:', spreadsheet)
     parser = parser_info.get('parser')
     print('parser:', parser)
     if parser:
